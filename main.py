58b7e60c8871f590dd56c17316052f1e2bf749d8
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-18 00:43:15 +0300

    updating main app

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+import routers
+import dependencies
+import routers.recipe
+import routers.users
+import routers.auth
 
 
-app = FastAPI()
+app = FastAPI(dependencies=[Depends(dependencies.get_query_token)])
 
+app.include_router(routers.recipe.router)
+app.include_router(routers.users.router)
+app.include_router(routers.auth.router)
 
 @app.get("/")
 def read_root():
