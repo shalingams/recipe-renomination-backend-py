0bafdd3497367c9faae45926ef81e9f24aa5942c
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-18 00:42:57 +0300

    Adding user routes

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+import dependencies
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    dependencies=[Depends(dependencies.get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
 
-@router.post("/update-account/{user_id}", tags=["users"])
+@router.put("/{user_id}", tags=["users"])
 def update_account(user_id: str):
     pass
